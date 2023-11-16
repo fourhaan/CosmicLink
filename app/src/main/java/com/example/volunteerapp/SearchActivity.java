f23ea4ac22c8c87b228dd0536f93b6a49bf8b88d
gantavyarohatgi <gantavya2022@gmail.com>
2023-11-17 01:24:02 +0530

    SearchActivity and SearchAdapter

--- app/src/main/java/com/example/volunteerapp/SearchActivity.java
+++ app/src/main/java/com/example/volunteerapp/SearchActivity.java
@@ -63,6 +63,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 // Handle errors
+
             }
         });
 
