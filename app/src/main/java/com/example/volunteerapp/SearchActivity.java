300de36dfe09526acf93e50025c54802a12d1799
gantavyarohatgi <gantavya2022@gmail.com>
2023-11-17 01:22:43 +0530

    SearchActivity and SearchAdapter

---
+++ app/src/main/java/com/example/volunteerapp/SearchActivity.java
@@ -0,0 +1,84 @@
+package com.example.volunteerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    RecyclerView searchRecyclerView;
+    SearchAdapter adapter;
+    FirebaseDatabase database;
+    ArrayList<Users> usersArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        DatabaseReference reference = database.getReference().child("Organisations");
+
+        usersArrayList = new ArrayList<>();
+
+        searchRecyclerView = findViewById(R.id.recyclerView);
+        searchRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new SearchAdapter(SearchActivity.this, usersArrayList);
+        searchRecyclerView.setAdapter(adapter);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersArrayList.clear(); // Clear the list before adding updated data
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    usersArrayList.add(users);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle errors
+            }
+        });
+
+        if (auth.getCurrentUser() == null) {
+            Intent intent = new Intent(SearchActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Finish this activity to prevent returning here after logging in
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
