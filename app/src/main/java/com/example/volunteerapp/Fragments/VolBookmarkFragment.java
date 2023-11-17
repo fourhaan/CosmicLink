adad424d02b9a048996e92d8f6c22931a292439c
nabhanshudash <dashnabhanshu@gmail.com>
2023-11-17 22:50:56 +0530

    Bookmark added

--- app/src/main/java/com/example/volunteerapp/Fragments/VolBookmarkFragment.java
+++ app/src/main/java/com/example/volunteerapp/Fragments/VolBookmarkFragment.java
@@ -3,19 +3,33 @@ package com.example.volunteerapp.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.volunteerapp.Models.modelPost;
 import com.example.volunteerapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
 
 public class VolBookmarkFragment extends Fragment {
+    FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    List<modelPost> postList;
+
+    public VolBookmarkFragment(){
+        //Empty constructor
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vol_bookmark, container, false);
+        View view =  inflater.inflate(R.layout.fragment_vol_bookmark, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
